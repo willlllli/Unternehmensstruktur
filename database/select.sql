097ff3f220c810dfdc2f9d66b8b868da7512d558
kentfuller <kfu396@proton.me>
2026-03-09 19:38:29 +0100

    fix OE fk Firma

--- database/select.sql
+++ database/select.sql
@@ -1,44 +1,6 @@
 /*
 SET search_path TO Unternehmensstruktur, public;
 
-select distinct Mitarbeiter.Standort_id
-from Mitarbeiter
-	inner join Mitglied on Personalnummer=Mitarbeiter
-	inner join
-		(select * from Organisationseinheit where Name='Squad-1-1-1-1')
-		on Organisationseinheit=Einheitsnummer
-order by standort_id;
-
-select Organisationseinheit, sum(Budget)
-from IT_Asset
-group by Organisationseinheit;
-
-select count(Bezeichnung)
-from Arbeitsplatz
-group by Standort_id;
-*/
-/*
-select Buchung.Standort_id, count(Buchung.Bezeichnung) as Auslastung, count(Arbeitsplatz.Bezeichnung) as Kapazitaet
-from Buchung inner join Arbeitsplatz on Buchung.Standort_id=Arbeitsplatz.standort_id and Buchung.Bezeichnung=Arbeitsplatz.Bezeichnung
-where Datum = '2026-03-02'
-group by Buchung.Standort_id;
-
-select
-  s.standort_id,
-  b.auslastung,
-  a.kapazitaet
-from Buerogebaeude s
-left join (
-  select Standort_id, count(*) as auslastung
-  from Buchung
-  group by Standort_id
-) b using (Standort_id)
-left join (
-  select Standort_id, count(*) as kapazitaet
-  from Arbeitsplatz
-  group by Standort_id
-) a using (Standort_id);
-
 Select Personalnummer, Name from Mitarbeiter where Standort_id = 2
 
 Select ICTO_Nummer, Name, Status, Risikostufe, Budget from IT_Asset where Kunde = 2
