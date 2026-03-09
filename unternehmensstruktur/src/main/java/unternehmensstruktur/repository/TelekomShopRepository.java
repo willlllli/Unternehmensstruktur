edeb99cc1a3c0dda401a54ecffde897a8d298c47
willlllli <willli@tutamail.com>
2026-03-09 17:16:12 +0100

    Remove: TelekomShopRepository.java (ersetzt durch FilialeRepository.java)

--- unternehmensstruktur/src/main/java/unternehmensstruktur/repository/TelekomShopRepository.java
+++
@@ -1,6 +0,0 @@
-package unternehmensstruktur.repository;
-
-import unternehmensstruktur.model.db.TelekomShop;
-
-public interface TelekomShopRepository extends BaseRepository<TelekomShop, Integer> {
-}
