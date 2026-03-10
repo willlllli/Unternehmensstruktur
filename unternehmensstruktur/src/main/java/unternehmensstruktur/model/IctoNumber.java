378e39e7ef84e63e956a85adbd45db6fc334d29d
willlllli <willli@tutamail.com>
2026-03-10 12:38:43 +0100

    fix: IctoNumber.java wiederhergestellt (wurde versehentlich geloescht)

---
+++ unternehmensstruktur/src/main/java/unternehmensstruktur/model/IctoNumber.java
@@ -0,0 +1,11 @@
+package unternehmensstruktur.model;
+
+/**
+ * Marker-Klasse fuer ICTO-Nummern im Format "ICTO-xxxx".
+ * Wird in SimpleSingleView verwendet, um ein TextField mit
+ * entsprechendem Placeholder und Regex-Validierung zu rendern
+ * statt eines IntegerField.
+ */
+public final class IctoNumber {
+    private IctoNumber() {}
+}
